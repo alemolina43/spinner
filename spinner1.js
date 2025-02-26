7599efa20640aa449d182ab7c58daab4c97dd428
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-25 16:17:48 -0800

    Eliminate repeated comments

--- spinner1.js
+++ spinner1.js
@@ -16,17 +16,14 @@ setTimeout(() => {
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|  ');
 }, 900);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r/  ');
 }, 1100);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r-  ');
 }, 1300);
 
